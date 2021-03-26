09f2e8ba0d8bff72d356693378d550090339fad5
basekat <dgk@tropot.net>
2021-03-26 18:36:26 +0100

    submission Bite 46 @ codechalleng.es

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,5 +1,2 @@
 def fizzbuzz(num):
-    return 'Fizz Buzz' if num % 15 == 0
-            else 'Fizz' if num % 3 == 0
-            else 'Buzz' if num % 5 == 0
-            else num
+    return 'Fizz Buzz' if num % 15 == 0 else 'Fizz' if num % 3 == 0 else 'Buzz' if num % 5 == 0 else num
